4595736442ee19281b008dd289a229e52e477097
Krzysztof Opasiak <k.opasiak@samsung.com>
2018-02-22 17:57:06 +0100

    libusbgx: tests: Add a C++ compilation checker
    Recently we faced some C++ compilation issues because
    we used in our header a gcc extension that is not supported
    by g++. Even through this problem has been fix let's try to
    avoid such problems in a future. To do so, let's add a C++
    compilation check for all our public headers as part of our
    test suit.
    
    Signed-off-by: Krzysztof Opasiak <k.opasiak@samsung.com>

---
+++ tests/header_checker.cpp
@@ -0,0 +1,20 @@
+/*
+ * This is just a dummy test which checks if all our headers
+ * compiles correctly with a C++ compiler.
+ */
+
+#include <usbg/usbg.h>
+#include <usbg/function/ffs.h>
+#include <usbg/function/hid.h>
+#include <usbg/function/loopback.h>
+#include <usbg/function/midi.h>
+#include <usbg/function/ms.h>
+#include <usbg/function/net.h>
+#include <usbg/function/phonet.h>
+#include <usbg/function/serial.h>
+#include <usbg/function/uac2.h>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
